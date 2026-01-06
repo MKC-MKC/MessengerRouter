72ea8906933792c9cc78f772c18bff5b2209684b
Haikiri <59623220+MKC-MKC@users.noreply.github.com>
2026-01-06 20:19:16 +0300

    Create First Test

---
+++ tests/CommandsRouterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\MessengerRouting;
+
+use PHPUnit\Framework\TestCase;
+use Tests\MessengerRouting\Mocked\Telegram;
+
+class CommandsRouterTest extends TestCase
+{
+
+	private array $update = [
+		"update_id" => 283756232,
+		"message" => [
+			"message_id" => 123,
+			"from" => [
+				"id" => 123456789,
+				"is_bot" => false,
+				"first_name" => "Иван Иванов",
+				"username" => "ivan_ivanov",
+			],
+			"chat" => [
+				"id" => -100123456789,
+				"title" => "Messenger Router",
+				"type" => "supergroup",
+			],
+			"date" => 1700000000,
+			"text" => "Some Message Text",
+		],
+	];
+
+	private static function process($controller, $update): bool
+	{
+		$controller->update = $update;
+		$controller->__invoke();
+		return (bool)$controller->wasCalled;
+	}
+
+	public function test_ThanksCommand(): void
+	{
+		$this->update["message"]["text"] = "Thank You!";
+		$this->assertTrue(self::process(new Telegram\ThanksCommand(), $this->update));
+
+		$this->update["message"]["text"] = "Спасибо!";
+		$this->assertTrue(self::process(new Telegram\ThanksCommand(), $this->update));
+	}
+
+}
