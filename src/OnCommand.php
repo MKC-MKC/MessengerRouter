74b5478f14ae9f627c243f5886dcef077e3cf45f
Haikiri <59623220+MKC-MKC@users.noreply.github.com>
2026-01-17 09:41:23 +0300

    @phpdoc: md fix

--- src/OnCommand.php
+++ src/OnCommand.php
@@ -10,16 +10,16 @@ class OnCommand
 	public array $commands;
 
 	/**
-	 * @param array|string $commands # Command or array of commands: "/command1" or ["/command1", "command2"].
-	 * @param bool $return_data # Whether the function should return the command data.
-	 * @param bool $require_data # Whether data is required. If required but missing - mismatch.
-	 * @param string $separator # Data separator, space by default. For example "/ban user1 2day". Use "_" for "/ban_user1_2d"
-	 * @param int|float $temperature # Text matching threshold percentage. Disabled by default when (100%).
-	 * @param bool $fuzzy # Allow matching a command anywhere within the full text string.
-	 * @param bool $botName # Allow to check the bot's name from your getter to support direct requests to your bot at /start@TungTungBot
-	 * @param bool $isOwner # Whether creator privileges are required to execute the command.
-	 * @param bool $isAdmin # Whether admin privileges are required to execute the command.
-	 * @param bool $isEnvAdmin # Allow the execution of the command only on behalf of the creators of the boat listed in your getter.
+	 * @param array|string $commands Command or array of commands: "/command1" or ["/command1", "command2"].
+	 * @param bool $return_data Whether the function should return the command data.
+	 * @param bool $require_data Whether data is required. If required but missing - mismatch.
+	 * @param string $separator Data separator, space by default. For example "/ban user1 2day". Use "_" for "/ban_user1_2d"
+	 * @param int|float $temperature Text matching threshold percentage. Disabled by default when (100%).
+	 * @param bool $fuzzy Allow matching a command anywhere within the full text string.
+	 * @param bool $botName Allow to check the bot's name from your getter to support direct requests to your bot at /start@TungTungBot
+	 * @param bool $isOwner Whether creator privileges are required to execute the command.
+	 * @param bool $isAdmin Whether admin privileges are required to execute the command.
+	 * @param bool $isEnvAdmin Allow the execution of the command only on behalf of the creators of the boat listed in your getter.
 	 */
 	public function __construct(
 		array|string     $commands,
